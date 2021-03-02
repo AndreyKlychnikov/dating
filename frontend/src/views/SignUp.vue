85e20a45750d24238a1484d70582812791aaf81f
andrew <off-rey@bk.ru>
2021-03-02 18:05:15 +0300

    First commit.

---
+++ frontend/src/views/SignUp.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-content>
+    <CreateUser/>
+  </v-content>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import CreateUser from "@/views/main/admin/CreateUser.vue";
+@Component({
+  components: {CreateUser}
+})
+export default class Login extends Vue {
+}
+</script>
+
+<style>
+</style>
