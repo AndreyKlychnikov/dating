cc9f64891a6ea92d4b2d9d0b2a30d537aa20b1cc
doker696 <doker696@users.noreply.github.com>
2021-06-15 19:15:38 +0300

    changed memory limit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,8 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 module.exports = {
   // Fix Vuex-typescript in prod: https://github.com/istrib/vuex-typescript/issues/13#issuecomment-409869231
   configureWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === "production") {
       config.optimization.minimizer[0].options.terserOptions = Object.assign(
         {},
         config.optimization.minimizer[0].options.terserOptions,
@@ -14,22 +15,40 @@ module.exports = {
           mangle: {
             keep_fnames: true,
           },
-        },
+        }
       );
+      const existingForkTsChecker = config.plugins.filter(
+        (p) => p instanceof ForkTsCheckerWebpackPlugin
+      )[0];
+
+      // remove the existing ForkTsCheckerWebpackPlugin
+      // so that we can replace it with our modified version
+      config.plugins = config.plugins.filter(
+        (p) => !(p instanceof ForkTsCheckerWebpackPlugin)
+      );
+
+      // copy the options from the original ForkTsCheckerWebpackPlugin
+      // instance and add the memoryLimit property
+      const forkTsCheckerOptions = existingForkTsChecker.options;
+      forkTsCheckerOptions.memoryLimit = 900;
+
+      config.plugins.push(new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions));
     }
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => Object.assign(options, {
-        transformAssetUrls: {
-          'v-img': ['src', 'lazy-src'],
-          'v-card': 'src',
-          'v-card-media': 'src',
-          'v-responsive': 'src',
-        }
-      }));
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) =>
+        Object.assign(options, {
+          transformAssetUrls: {
+            "v-img": ["src", "lazy-src"],
+            "v-card": "src",
+            "v-card-media": "src",
+            "v-responsive": "src",
+          },
+        })
+      );
   },
-}
+};
