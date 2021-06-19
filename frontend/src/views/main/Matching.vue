b56062e66b0ce96fb606fa943dddc52ae47c93c5
doker696 <doker696@users.noreply.github.com>
2021-06-19 23:11:10 +0300

    added filter overlay

--- frontend/src/views/main/Matching.vue
+++ frontend/src/views/main/Matching.vue
@@ -1,36 +1,84 @@
 <template>
   <v-app>
     <div class="mx-auto">
-    <div>
-     <v-img
-     v-if="userProfileById && userProfileById.avatar"
-        class="my-4 mx-auto"
-        :src="`http://localhost/static/${userProfileById.avatar}`"
-        width="450"
-        height="550"
-        
-      ></v-img>
-      <v-img
-        v-else
-        class="my-4 mx-auto"
-        src="https://oionline.com/wp-content/uploads/2018/03/not_found.png"
-        width="450"
-        height="450"
-        
-      ></v-img>
-      <h2 >{{ currentUserNotShown.full_name }} {{userProfileById.age? userProfileById.age: ""}} </h2>
-      <div style="max-width:500px">
-      <h2 >{{ userProfileById && userProfileById.description || "No description :( "}}</h2>
+      <div
+        v-if="userProfile.avatar && userProfile.description && userProfile.age"
+      >
+        <div>
+          <v-img
+            v-if="currentUserNotShown && currentUserNotShown.avatar"
+            class="my-4 mx-auto"
+            :src="`http://localhost/static/${currentUserNotShown.avatar}`"
+            width="450"
+            height="550"
+          ></v-img>
+          <v-img
+            v-else
+            class="my-4 mx-auto"
+            src="https://oionline.com/wp-content/uploads/2018/03/not_found.png"
+            width="450"
+            height="450"
+          ></v-img>
+          <h2>
+            {{ userProfile.full_name }}
+            {{ currentUserNotShown.age ? currentUserNotShown.age : "" }}
+          </h2>
+          <div style="max-width: 500px">
+            <h2>
+              {{
+                (currentUserNotShown && currentUserNotShown.description) ||
+                "No description :( "
+              }}
+            </h2>
+          </div>
+        </div>
+        <v-row :justify="'space-between'" class="my-2">
+          <v-btn c @click="like" icon
+            ><v-icon color="red">mdi-heart</v-icon></v-btn
+          >
+          <v-btn @click="overlay = !overlay" icon
+            ><v-icon color="black">mdi-cog</v-icon></v-btn
+          >
+          <v-btn @click="notlike" icon
+            ><v-icon color="black">mdi-close</v-icon></v-btn
+          >
+        </v-row>
       </div>
-      </div>
-      <v-row :justify="'space-between'" class="my-2">
-        <v-btn c @click="like" icon
-          ><v-icon color="red">mdi-heart</v-icon></v-btn
+      <h2 v-else>Please Update your profile!</h2>
+      <v-overlay :absolute="false" :value="overlay">
+        <v-sheet
+          color="white"
+          class="my-auto"
+          elevation="1"
+          height="250"
+          width="500"
         >
-        <v-btn @click="notlike" icon
-          ><v-icon color="black">mdi-close</v-icon></v-btn
-        >
-      </v-row>
+          <v-container>
+            <div class="my-auto">
+              <v-select
+                light
+                :items="['Not set', 'Male', 'Female']"
+                label="Prefered gender"
+                v-model="preferred_gender_str"
+              ></v-select>
+              <v-range-slider max="100" min="0" v-model="preferred_age" light>
+                <template v-slot:prepend>
+                  <div class="mt-0 pt-0 text-caption">
+                    {{ preferred_age[0] }}
+                  </div>
+                </template>
+                <template v-slot:append>
+                  <div class="mt-0 pt-0 text-caption">
+                    {{ preferred_age[1] }}
+                  </div>
+                </template>
+              </v-range-slider>
+              <v-btn @click="overlay = false" class="mx-2" light> Close </v-btn>
+              <v-btn color="success" @click="save" light> Save </v-btn>
+            </div>
+          </v-container>
+        </v-sheet>
+      </v-overlay>
     </div>
   </v-app>
 </template>
@@ -38,30 +86,40 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { Store } from "vuex";
-import { readUserProfile, readUserProfileNotShown, readUserProfileById } from "@/store/main/getters";
-import { ISendSympathy } from "@/interfaces";
+import {
+  readUserProfile,
+  readUserProfileNotShown,
+  readUserProfileById,
+} from "@/store/main/getters";
+import { ISendSympathy, IUserProfileUpdate } from "@/interfaces";
 import {
   dispatchUserLogOut,
   dispatchGetUserProfileNotShown,
   dispatchGetUserProfileById,
   dispatchSympathy,
+  dispatchUpdateUserProfile,
 } from "@/store/main/actions";
+import UserProfile from "../admin/profile/UserProfile.vue";
 
 @Component
 export default class Matching extends Vue {
   get userProfile() {
     return readUserProfile(this.$store);
   }
-  get userProfileById() {
-    return readUserProfileById(this.$store);
-  }
   get userProfileNotShown() {
     return readUserProfileNotShown(this.$store);
   }
-  get currentUserNotShown(){
+  get currentUserNotShown() {
     return this.userProfileNotShown![this.current];
   }
-  
+  public overlay: boolean = false;
+  public preferred_gender_str: string = "";
+  public preferred_gender: boolean | null = this.userProfile.preferred_gender;
+  public preferred_age: number[] | null = [
+    this.userProfile.preferred_age_min,
+    this.userProfile.preferred_age_max,
+  ];
+
   current: number = 0;
   public like() {
     const user = {
@@ -69,30 +127,37 @@ export default class Matching extends Vue {
     };
     dispatchSympathy(this.$store, user as ISendSympathy);
     this.current++;
-    dispatchGetUserProfileById(this.$store,this.userProfileNotShown![this.current]!.id!);
-
-
   }
+  public save() {
+    let gen = null;
+    if (this.preferred_gender_str == "Male") {
+      gen = true;
+    } else if (this.preferred_gender_str == "Female") {
+      gen = false;
+    }
 
+    const profile: IUserProfileUpdate = {
+      preferred_gender: gen,
+      preferred_age_min: this.preferred_age[0],
+      preferred_age_max: this.preferred_age[1],
+    };
+
+    dispatchUpdateUserProfile(this.$store, profile);
+    dispatchGetUserProfileNotShown(this.$store);
+
+    this.overlay = false;
+  }
   public notlike() {
-      if(this.current + 1 < this.userProfileNotShown!.length){
-
-        this.current++;
-
-        dispatchGetUserProfileById(this.$store,this.currentUserNotShown!.id!);
-      } else {
-        this.current = 0;
-        dispatchGetUserProfileNotShown(this.$store);
-      }
-
+    if (this.current + 1 < this.userProfileNotShown!.length) {
+      this.current++;
+    } else {
+      this.current = 0;
+      dispatchGetUserProfileNotShown(this.$store);
+    }
   }
-
+  public updateFilter() {}
   mounted() {
     dispatchGetUserProfileNotShown(this.$store);
-    setTimeout(()=>{
-      dispatchGetUserProfileById(this.$store,this.currentUserNotShown!.id!);
-    },100)
-
   }
 }
 </script>
